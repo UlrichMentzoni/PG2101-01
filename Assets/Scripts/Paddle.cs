8dd22b05a7529df2e24d23cdd5d6f344028dcdcc
BenteO <ottben14@student.westerdals.no>
2015-03-03 20:47:55 +0100

    Fjernet kommentar som var der fra før av

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -6,9 +6,7 @@ public class Paddle : MonoBehaviour {
 	public float paddleSpeed;
 
 	private Vector3 playerPos = new Vector3(0f, -3.17f, 0f);
-	
 
-	// Update is called once per frame
 	void Update () {
 	
 		float xPos = transform.position.x + (Input.GetAxis ("Horizontal") * paddleSpeed * Time.deltaTime);
