89dca23c73ded390612192845c1623b62c51cca9
BenteO <ottben14@student.westerdals.no>
2015-03-07 12:41:48 +0100

    Update GM script

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class GM : MonoBehaviour {
 	
 	public int lives = 3;
-	public int bricks = 20;
+	public int bricks = 176;
 	public float resetDelay = 1f;
 	public Text livesText;
 	public GameObject gameOver;
@@ -17,7 +17,7 @@ public class GM : MonoBehaviour {
 	
 	private GameObject clonePaddle;
 	
-	// Use this for initialization
+	// Use this for initialization, checks if there is an existing game and destroys it
 	void Awake () 
 	{
 		if (instance == null)
@@ -29,22 +29,24 @@ public class GM : MonoBehaviour {
 		
 	}
 	
+	// Starts the game and spawns the objects
 	public void Setup()
 	{
 		clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity) as GameObject;
 		Instantiate(bricksPrefab, transform.position, Quaternion.identity);
 	}
 	
+	// Checks if you won the game or not
 	void CheckGameOver()
 	{
-		if (bricks < 1)
+		if (bricks < 1) // You won
 		{
 			youWon.SetActive(true);
 			Time.timeScale = .25f;
 			Invoke ("Reset", resetDelay);
 		}
 		
-		if (lives < 1)
+		if (lives < 1) // You lose
 		{
 			gameOver.SetActive(true);
 			Time.timeScale = .25f;
@@ -53,22 +55,29 @@ public class GM : MonoBehaviour {
 		
 	}
 	
+	// Starts the level again
 	void Reset()
 	{
 		Time.timeScale = 1f;
 		Application.LoadLevel(Application.loadedLevel);
 	}
 	
+	// Keeps score of numbero f lives
 	public void LoseLife()
 	{
 		lives--;
 		livesText.text = "Lives: " + lives;
+		
+		// Destroys paddle
 		Instantiate(deathParticles, clonePaddle.transform.position, Quaternion.identity);
 		Destroy(clonePaddle);
+		
+		// Spawns the paddle again
 		Invoke ("SetupPaddle", resetDelay);
 		CheckGameOver();
 	}
 	
+	// Spawns the paddle
 	void SetupPaddle()
 	{
 		clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity) as GameObject;
